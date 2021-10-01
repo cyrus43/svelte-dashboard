9d8810bd34cdcb6d80693a0c632d2024295a2514
Cyrus Mbithi <cyrusmbithi92@gmail.com>
2021-10-01 10:53:26 +0300

    update server changes

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const posts = [
 				<li>Code-splitting, dynamic imports and hot module replacement, powered by webpack</li>
 				<li>Server-side rendering (SSR) with client-side hydration</li>
 				<li>Service worker for offline support, and all the PWA bells and whistles</li>
-				<li>The nicest development experience you've ever had, or your money back</li>
+				<li>The nicest development experience you've ever had, or your money back or both </li>
 			</ul>
 
 			<p>It's implemented as Express middleware. Everything is set up and waiting for you to get started, but you keep complete control over the server, service worker, webpack config and everything else, so it's as flexible as you need it to be.</p>
